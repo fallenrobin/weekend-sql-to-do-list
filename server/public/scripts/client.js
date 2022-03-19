ce2ce3fe9c67c4a903a6581595c22fea5e78da21
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-19 16:00:54 -0500

    add input elements and table for render

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,20 +2,37 @@ $(onReady);
 
 function onReady() {
     console.log('ready!');
-connectToDB();
+getTasks();
+// $('.dropdown-toggle').dropdown();
 }
 
-function connectToDB(){
+
+
+// Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+
+function getTasks(){
     console.log( 'in connect to DB' );
-    // ajax call to server to get koalas
+    // ajax call to server to get tasks from DB
     $.ajax({
       type: 'GET',
       url: '/todo' 
     }).then(function(response) {
       console.log(response);
-    //   renderKoalas(response); // Still needs to be created
+    //   renderTasks(response); //to append current tasks on DOM
     }).catch(function(error){
       console.log('error in GET', error);
     });
   
-  } // end getKoalas
+  }
