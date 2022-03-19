8b847b2efb19740be32e04aef47114f854983698
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-03-19 16:07:31 -0500

    add render on page load

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,9 @@
 $(onReady);
 
 function onReady() {
-    console.log('ready!');
-getTasks();
-// $('.dropdown-toggle').dropdown();
+  console.log('ready!');
+  getTasks();
+  // $('.dropdown-toggle').dropdown();
 }
 
 
@@ -22,17 +22,53 @@ getTasks();
 //     }
 //   }
 
-function getTasks(){
-    console.log( 'in connect to DB' );
-    // ajax call to server to get tasks from DB
-    $.ajax({
-      type: 'GET',
-      url: '/todo' 
-    }).then(function(response) {
-      console.log(response);
-    //   renderTasks(response); //to append current tasks on DOM
-    }).catch(function(error){
-      console.log('error in GET', error);
-    });
-  
+function getTasks() {
+  console.log('in connect to DB');
+  // ajax call to server to get tasks from DB
+  $.ajax({
+    type: 'GET',
+    url: '/todo'
+  }).then(function (response) {
+    console.log(response);
+    renderTasks(response); //to append current tasks on DOM
+  }).catch(function (error) {
+    console.log('error in GET', error);
+  });
+
+}
+
+function renderTasks(tasks) {
+  $('#viewTasks').empty();
+
+  for (let i = 0; i < tasks.length; i += 1) {
+    let task = tasks[i];
+
+    if (task.completed === false) {
+      let row = $(`
+          <tr>
+              <td class="priorityClass highPriority">hardcoded as highPriority</td>
+              <td>hardcoded task!</td>
+              <td>hardcoded home</td>
+              <td><button class="btn btn-success completeBtn">Got 'er done!</button></td>
+              <td><button class="btn btn-danger deleteBtn">Remove</button></td>
+        </tr>
+          `);
+      row.data('task', task);
+      $('#viewTasks').append(row);
+      console.log(row.data('task'));
+    } else {
+      let row = $(`
+      <tr>
+              <td class="priorityClass highPriority">hardcoded as highPriority</td>
+              <td>hardcoded task!</td>
+              <td>hardcoded home</td>
+              <td></td>
+              <td><button class="btn btn-danger deleteBtn">Remove</button></td>
+      </tr>
+        `);
+      row.data('task', task);
+      $('#viewTasks').append(row);
+      console.log(row.data('task'));
+    }
   }
+}
